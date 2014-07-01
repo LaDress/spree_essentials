a80117d775408c98284c4b9b626db83438839711
coupling <martijn@coupling.nl>
2014-07-01 14:46:49 +0200

    Fixes menu, look and feel.

--- app/overrides/admin/spree_essentials.rb
+++ app/overrides/admin/spree_essentials.rb
@@ -5,9 +5,3 @@ Deface::Override.new(:virtual_path  => "spree/admin/shared/_menu",
                      :partial       => "spree/admin/shared/contents_tab",
                      :disabled      => false)
 
-# Adds the contents menu. Inside the contents sub menu we ensure it's needed
-Deface::Override.new(:virtual_path  => "spree/admin/shared/_sub_menu",
-                     :name          => "spree_essential_contents_menu",
-                     :insert_top    => "#sub-menu[data-hook]",
-                     :partial       => "spree/admin/shared/contents_sub_menu",
-                     :disabled      => false)
